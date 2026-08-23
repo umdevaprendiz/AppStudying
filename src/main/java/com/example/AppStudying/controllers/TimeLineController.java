25820e677210d7fb23b7a20dc16d8a51e91453e4
Sérgio Guilherme <187855489+umdevaprendiz@users.noreply.github.com>
2026-08-22 22:02:00 -0300

    Terminando TimeLineController

--- src/main/java/com/example/AppStudying/controllers/TimeLineController.java
+++ src/main/java/com/example/AppStudying/controllers/TimeLineController.java
@@ -1,4 +1,28 @@
 package com.example.AppStudying.controllers;
 
+import com.example.AppStudying.model.TimeLine;
+import com.example.AppStudying.services.TimeLineService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/TimeLine")
 public class TimeLineController {
+
+    @Autowired
+    private TimeLineService timeLineService;
+
+    @PostMapping("/criarEvento")
+    public TimeLine criarEvento(@RequestParam String description, @RequestParam Long userId){
+        return timeLineService.criarEvento(description, userId);
+    }
+
+    @GetMapping("/listarUsuarios")
+    public List<TimeLine> listarPorUsuario(@RequestParam Long userId){
+        return timeLineService.listarPorUsuario(userId);
+    }
+
+
 }
