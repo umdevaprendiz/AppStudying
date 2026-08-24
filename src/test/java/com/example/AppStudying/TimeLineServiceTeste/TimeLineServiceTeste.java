8f6157a6244574d4ae8597732251c9a7ad3dc564
Sérgio Guilherme <187855489+umdevaprendiz@users.noreply.github.com>
2026-08-23 21:16:32 -0300

    terminando TimeLineServiceTeste, faltando apenas 2 testes

--- src/test/java/com/example/AppStudying/TimeLineServiceTeste/TimeLineServiceTeste.java
+++ src/test/java/com/example/AppStudying/TimeLineServiceTeste/TimeLineServiceTeste.java
@@ -11,11 +11,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions. assertNotNull;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -57,4 +57,46 @@ public class TimeLineServiceTeste {
 
     }
 
+    @Test
+    void criarEventoUsuarioNaoEncontrado(){
+        Long userId = 99L;
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        assertThrows(IllegalStateException.class, () ->{
+            timeLineService.criarEvento("Anythings", userId);
+        });
+
+        verify(timeLineRepository, never()).save(any(TimeLine.class));
+
+    }
+
+    @Test
+    void listarPorUsuarioRetornaLista(){
+        Long userId = 1L;
+
+        TimeLine timeLine1 = new TimeLine();
+        timeLine1.setId(1L);
+        timeLine1.setDescription("Estudo cálculo 1");
+
+        TimeLine timeLine2 = new TimeLine();
+        timeLine2.setId(2L);
+        timeLine2.setDescription("Estudando Programação");
+        when(timeLineRepository.findByUserId(userId)).thenReturn(List.of(timeLine1, timeLine2 ));
+
+        List<TimeLine> resultado = timeLineService.listarPorUsuario(userId);
+
+        assertEquals(2, resultado.size());
+        verify(timeLineRepository, times(1)).findByUserId(userId);
+
+    }
+
+    @Test
+    void buscarIdNaoEncontrado(){
+        Long Id = 88L;
+
+        when(timeLineRepository.findById(Id)).thenReturn(Optional.empty());
+
+        //continuando...
+
+    }
 }
